c304fb2d266698d7311f577d9a3b612328da63cd
liamgen.js <liamgen.js@proton.me>
2024-08-20 17:31:39 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Diagnostics;
-using System.Collections.Generic;
-using System.Net.Http;
-using System.Threading.Tasks;
 using Newtonsoft.Json;
 public class ProcessKillList
 {
